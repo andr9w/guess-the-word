86fece483c69a5244c7a8942fa7a3a32a1f1d4e6
andr9w <apaik@me.com>
2022-04-06 18:46:25 -0400

    Accept and validate guesses

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const guessedLetters = document.querySelector(".guessed-letters");
+const guessedLettersElement = document.querySelector(".guessed-letters");
 const guessButton = document.querySelector(".guess");
 const letterInput = document.querySelector(".letter");
 const wordInProgress = document.querySelector(".word-in-progress");
@@ -8,6 +8,7 @@ const message = document.querySelector(".message");
 const playAgainButton = document.querySelector(".play-again");
 
 const word = "magnolia";
+const guessedLetters = [];
 
 const placeholder = function (word) {
     const placeholderLetters = [];
@@ -22,7 +23,42 @@ placeholder(word);
 
 guessButton.addEventListener("click", function (e) {
     e.preventDefault();
+    // Empty message paragraph
+    message.innerText = "";
+    // Let's grab what was entered in the input
     const guess = letterInput.value;
-    console.log(guess);
+    // Let's make sure that it is a single letter
+    const goodGuess = inputValidate(guess);
+
+    if (goodGuess) {
+        // We've got a letter! Let's guess!
+        makeGuess(guess);
+    }
     letterInput.value = "";
 });
+
+const inputValidate = function (input) {
+    const acceptedLetter = /[a-zA-Z]/;
+    if (input.length === 0) {
+        // Is the input empty?
+        message.innerText = "Please input a letter.";
+    } else if (input.length > 1) {
+        // Did you type more than one letter?
+        message.innerText = "Please input only 1 letter.";
+    } else if (!input.match(acceptedLetter)) {
+        // Did you type a number, a special character, or non letter?
+        message.innerText = "Please enter a letter from A to Z.";
+    } else {
+        return input;
+    };
+};
+
+const makeGuess = function (guess) {
+    guess = guess.toUppercase();
+    if (guessedLetters.includes(guess)) {
+        message.innerText = "You already guessed that letter, silly. Try again.";
+    } else {
+        guessedLetters.push(guess);
+        console.log(guessedLetters);
+    }
+};
